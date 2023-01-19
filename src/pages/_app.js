38334ed00c3348db3829b7e9597dd9ba1e0fe7e9
AnasAito <khdimaty@gmail.com>
2023-01-19 15:21:10 +0100

    add packages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@tremor/react/dist/esm/tremor.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
